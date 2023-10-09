9b322fc0f32567fcf11d61a56d4bddcd2e304262
xikong-mubai <kirito_xibai@qq.com>
2023-10-10 02:13:11 +0800

    coding

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
-import win32gui,win32ui,win32con,win32com.client,random
+import win32gui,win32ui,win32con,win32com.client,random,win32api,win32print
 from PIL import Image
+from ctypes import windll
 import pyautogui,time,pythoncom
 # 获取权限
 import ctypes, sys, os
@@ -19,36 +20,79 @@ def is_admin():
 # 生成随机数
 def rand_num(x, y):
     return round(random.uniform(x, y),3)
+ 
+def get_system_dpi():
+    """获取缩放后的分辨率"""
+    hdc = win32gui.GetDC(0)
+    x_dpi: int = win32print.GetDeviceCaps(hdc, win32con.LOGPIXELSX)
+    # y_dpi = win32print.GetDeviceCaps(hdc, win32con.LOGPIXELSY)
+    screen_scale_rate=x_dpi/96
+    return screen_scale_rate
+ 
+if __name__=="__main__":
+    a=get_system_dpi()
+    print(a)
+
+# 检测 阴阳师 窗口比例是否更新
+def check_window(dst: Image):
+    img_x,img_y = dst.size
+
+    tmp = Image.open('./123.png')
+    tmp_img_x,tmp_img_y = tmp.size
+    if img_x != tmp_img_x or img_y != tmp_img_y:
+        print("please update img's image!")
+        print("Press enter to close")
+        input()
+        exit()
 
 # 获取阴阳师运行状态
 def get_windows(windowsname,filename):
     try:
-        # 获取窗口句柄
-        yys_handle = win32gui.FindWindow(None,windowsname)
-        # 控制窗口的位置和大小 
-        # 参数1：控制的窗体
-        # 参数2：大致方位,HWND_TOPMOST上方
-        # 参数3：位置x
-        # 参数4：位置y
-        # 参数5：长度
-        # 参数6：宽度
         img = Image.open("img/room_wait.png")
         tmp_img_x,tmp_img_y = img.size
-        win32gui.SetWindowPos(yys_handle, win32con.HWND_NOTOPMOST, 0, 0, tmp_img_x, tmp_img_y, win32con.SWP_SHOWWINDOW)#win32con.SWP_NOACTIVATE|win32con.SWP_SHOWWINDOW)
         
-        time.sleep(0.3)
-        pythoncom.CoInitialize()
-        shell = win32com.client.Dispatch("WScript.Shell")
-        shell.SendKeys('%')
-        # 将窗口放在前台，并激活该窗口（窗口不能最小化）
-        #win32gui.SetForegroundWindow(yys_handle)
-        time.sleep(0.3)
-        pythoncom.CoInitialize()
-        shell = win32com.client.Dispatch("WScript.Shell")
-        shell.SendKeys('%')
+        print('get window data...')
+        # 获取窗口句柄
+        print(win32gui.EnumWindows)
+        yys_handle = win32gui.FindWindow(None,windowsname)
+        print("get window hanlde")
+        # time.sleep(0.3)
+        # pythoncom.CoInitialize()
+        # shell = win32com.client.Dispatch("WScript.Shell")
+        # shell.SendKeys('%')
+        # # 将窗口放在前台，并激活该窗口（窗口不能最小化）
+        # #win32gui.SetForegroundWindow(yys_handle)
+        # time.sleep(0.3)
+        # pythoncom.CoInitialize()
+        # shell = win32com.client.Dispatch("WScript.Shell")
+        # shell.SendKeys('%')
+        
         # 获取窗口DC
-        win32gui.EnumWindows
+        #win32gui.EnumWindows
         yys_hdDC = win32gui.GetWindowDC(yys_handle)
+        print("get window DC")
+        
+        # 获取窗口的位置信息
+        left, top, right, bottom = win32gui.GetWindowRect(yys_handle)
+        print(left, top, right, bottom,tmp_img_x,tmp_img_y)
+        if left < 0 or top < 0:
+            print("\a请把阴阳师窗口打开至桌面（不能最小化）")
+        # 窗口长宽
+        width = right - left
+        height = bottom - top
+        print()
+        if width != tmp_img_x or tmp_img_y != height:
+            # 将窗口放在前台，并激活该窗口（窗口不能最小化）
+            #win32gui.SetForegroundWindow(yys_handle)
+
+            # 控制窗口的位置和大小 
+            # 参数1：控制的窗体
+            # 参数2：大致方位,HWND_TOPMOST上方
+            # 参数3：位置x
+            # 参数4：位置y
+            # 参数5：长度
+            # 参数6：宽度
+            win32gui.SetWindowPos(yys_handle, win32con.HWND_NOTOPMOST, 0, 0, tmp_img_x, tmp_img_y, win32con.SWP_SHOWWINDOW)#win32con.SWP_NOACTIVATE|win32con.SWP_SHOWWINDOW)
         
         # 根据句柄创建一个DC
         newhdDC = win32ui.CreateDCFromHandle(yys_hdDC)
@@ -57,13 +101,6 @@ def get_windows(windowsname,filename):
         # 创建bitmap保存图片
         saveBitmap = win32ui.CreateBitmap()
 
-        # 获取窗口的位置信息
-        left, top, right, bottom = win32gui.GetWindowRect(yys_handle)
-        if left < 0 or top < 0:
-            print("\a请把阴阳师窗口打开至桌面（不能最小化）")
-        # 窗口长宽
-        width = right - left
-        height = bottom - top
         # bitmap初始化
         saveBitmap.CreateCompatibleBitmap(newhdDC, width, height)
         saveDC.SelectObject(saveBitmap)

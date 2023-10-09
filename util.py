4971cd20cbbc9c5e1a4fc3dd1d42e289ef35b15b
xikong-mubai <kirito_xibai@qq.com>
2023-10-09 17:41:27 +0800

    coding optimization

---
+++ util.py
@@ -0,0 +1,79 @@
+import win32gui,win32ui,win32con,win32com.client,random
+from PIL import Image
+import pyautogui,time,pythoncom
+# 获取权限
+import ctypes, sys, os
+
+def help():
+    print('1. 魂土')
+    print('2. 御灵')
+    print('0. 退出')
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except Exception as e:
+        print(e)
+        return False
+
+# 生成随机数
+def rand_num(x, y):
+    return round(random.uniform(x, y),3)
+
+# 获取阴阳师运行状态
+def get_windows(windowsname,filename):
+    #try:
+        # 获取窗口句柄
+        yys_handle = win32gui.FindWindow(None,windowsname)
+        # 控制窗口的位置和大小 
+        # 参数1：控制的窗体
+        # 参数2：大致方位,HWND_TOPMOST上方
+        # 参数3：位置x
+        # 参数4：位置y
+        # 参数5：长度
+        # 参数6：宽度
+        img = Image.open("img/room_wait.png")
+        tmp_img_x,tmp_img_y = img.size
+        win32gui.SetWindowPos(yys_handle, win32con.HWND_NOTOPMOST, 0, 0, tmp_img_x, tmp_img_y, win32con.SWP_SHOWWINDOW)#win32con.SWP_NOACTIVATE|win32con.SWP_SHOWWINDOW)
+        
+        time.sleep(0.3)
+        pythoncom.CoInitialize()
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        # 将窗口放在前台，并激活该窗口（窗口不能最小化）
+        #win32gui.SetForegroundWindow(yys_handle)
+        time.sleep(0.3)
+        pythoncom.CoInitialize()
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.SendKeys('%')
+        # 获取窗口DC
+        win32gui.EnumWindows
+        yys_hdDC = win32gui.GetWindowDC(yys_handle)
+        
+        # 根据句柄创建一个DC
+        newhdDC = win32ui.CreateDCFromHandle(yys_hdDC)
+        # 创建一个兼容设备内存的DC
+        saveDC = newhdDC.CreateCompatibleDC()
+        # 创建bitmap保存图片
+        saveBitmap = win32ui.CreateBitmap()
+
+        # 获取窗口的位置信息
+        left, top, right, bottom = win32gui.GetWindowRect(yys_handle)
+        if left < 0 or top < 0:
+            print("\a请把阴阳师窗口打开至桌面（不能最小化）")
+        # 窗口长宽
+        width = right - left
+        height = bottom - top
+        # bitmap初始化
+        saveBitmap.CreateCompatibleBitmap(newhdDC, width, height)
+        saveDC.SelectObject(saveBitmap)
+        time.sleep(0.3)
+        saveDC.BitBlt((0, 0), (width, height), newhdDC, (left, top), win32con.SRCCOPY)
+        time.sleep(0.3)
+        saveBitmap.SaveBitmapFile(saveDC, filename)
+        win32gui.DeleteObject(saveBitmap.GetHandle())
+        saveDC.DeleteDC()
+        newhdDC.DeleteDC()
+        win32gui.ReleaseDC(yys_handle,yys_hdDC)
+    #except Exception as error:
+    #    print("get_window error!!!\n",error)
--- yys.py
+++ yys.py
@@ -1,78 +1,9 @@
+import win32gui,win32ui,win32con,win32com.client,random
 from PIL import Image
-import win32gui,win32ui,win32con,win32com.client
-import pyautogui,random,time,pythoncom
-
+import pyautogui,time,pythoncom
 # 获取权限
 import ctypes, sys, os
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-
-
-# 生成随机数
-def randNum(x, y):
-    return round(random.uniform(x, y),3)
-
-# 获取阴阳师运行状态
-def get_windows(windowsname,filename):
-    #try:
-        # 获取窗口句柄
-        yys_handle = win32gui.FindWindow(None,windowsname)
-        # 控制窗口的位置和大小 
-        # 参数1：控制的窗体
-        # 参数2：大致方位,HWND_TOPMOST上方
-        # 参数3：位置x
-        # 参数4：位置y
-        # 参数5：长度
-        # 参数6：宽度
-        img = Image.open("img/room_wait.png")
-        tmp_img_x,tmp_img_y = img.size
-        win32gui.SetWindowPos(yys_handle, win32con.HWND_NOTOPMOST, 0, 0, tmp_img_x, tmp_img_y, win32con.SWP_SHOWWINDOW)#win32con.SWP_NOACTIVATE|win32con.SWP_SHOWWINDOW)
-        
-        time.sleep(0.3)
-        pythoncom.CoInitialize()
-        shell = win32com.client.Dispatch("WScript.Shell")
-        shell.SendKeys('%')
-        # 将窗口放在前台，并激活该窗口（窗口不能最小化）
-        #win32gui.SetForegroundWindow(yys_handle)
-        time.sleep(0.3)
-        pythoncom.CoInitialize()
-        shell = win32com.client.Dispatch("WScript.Shell")
-        shell.SendKeys('%')
-        # 获取窗口DC
-        win32gui.EnumWindows
-        yys_hdDC = win32gui.GetWindowDC(yys_handle)
-        
-        # 根据句柄创建一个DC
-        newhdDC = win32ui.CreateDCFromHandle(yys_hdDC)
-        # 创建一个兼容设备内存的DC
-        saveDC = newhdDC.CreateCompatibleDC()
-        # 创建bitmap保存图片
-        saveBitmap = win32ui.CreateBitmap()
-
-        # 获取窗口的位置信息
-        left, top, right, bottom = win32gui.GetWindowRect(yys_handle)
-        if left < 0 or top < 0:
-            print("\a请把阴阳师窗口打开至桌面（不能最小化）")
-        # 窗口长宽
-        width = right - left
-        height = bottom - top
-        # bitmap初始化
-        saveBitmap.CreateCompatibleBitmap(newhdDC, width, height)
-        saveDC.SelectObject(saveBitmap)
-        time.sleep(0.3)
-        saveDC.BitBlt((0, 0), (width, height), newhdDC, (left, top), win32con.SRCCOPY)
-        time.sleep(0.3)
-        saveBitmap.SaveBitmapFile(saveDC, filename)
-        win32gui.DeleteObject(saveBitmap.GetHandle())
-        saveDC.DeleteDC()
-        newhdDC.DeleteDC()
-        win32gui.ReleaseDC(yys_handle,yys_hdDC)
-    #except Exception as error:
-    #    print("get_window error!!!\n",error)
+from .util import is_admin,get_windows
 
 # 挖土
 def watu():
@@ -126,13 +57,13 @@ def watu():
             sum[2] += abs(tmp_pixel[2] - room_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
             tmp_pixel = tmp_img.getpixel((int(0.948 * img_x),int(0.882 * img_y)))
             
             if tmp_pixel[0] == tmp_pixel[1] and tmp_pixel[1] == tmp_pixel[2]:
                 pass
             else:
-                pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+                pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
                 num += 1
                 time.sleep(23)
         
@@ -146,8 +77,8 @@ def watu():
             sum[2] += abs(tmp_pixel[2] - war1_pixel[2])
         
         if abs(sum[0] / 9 < 10) and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
         
         # 判断是否是结束二阶段
@@ -160,8 +91,8 @@ def watu():
             sum[2] += abs(tmp_pixel[2] - war2_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
 
         # 判断是否是 defeat
@@ -174,8 +105,8 @@ def watu():
             sum[2] += abs(tmp_pixel[2] - defeat_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
         
         print('\r这是第'+str(num)+'次',end='')
@@ -254,8 +185,8 @@ def yuling():
             sum[2] += abs(tmp_pixel[2] - room_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.8987 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.872 * img_x),int(0.925 * img_x)), y=randNum(int(0.85 * img_y),int(0.932 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.8987 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.872 * img_x),int(0.925 * img_x)), y=rand_num(int(0.85 * img_y),int(0.932 * img_y)))
             num += 1
             time.sleep(10)
         
@@ -269,8 +200,8 @@ def yuling():
             sum[2] += abs(tmp_pixel[2] - war1_pixel[2])
         
         if abs(sum[0] / 9 < 10) and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
         
         # 判断是否是结束二阶段
@@ -283,8 +214,8 @@ def yuling():
             sum[2] += abs(tmp_pixel[2] - war2_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
         
         # 判断是否是 defeat
@@ -297,8 +228,8 @@ def yuling():
             sum[2] += abs(tmp_pixel[2] - defeat_pixel[2])
         
         if abs(sum[0] / 9) < 10 and abs(sum[1] / 9) < 10 and abs(sum[2] / 10) < 10:
-            # pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=randNum(1,4), button='left', duration=randNum(1,3), tween=pyautogui.linear)
-            pyautogui.click(x=randNum(int(0.923 * img_x),int(0.9615 * img_x)), y=randNum(int(0.8551 * img_y),int(0.924 * img_y)))
+            # pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)), clicks=1, interval=rand_num(1,4), button='left', duration=rand_num(1,3), tween=pyautogui.linear)
+            pyautogui.click(x=rand_num(int(0.923 * img_x),int(0.9615 * img_x)), y=rand_num(int(0.8551 * img_y),int(0.924 * img_y)))
             time.sleep(0.3)
 
         print('\r这是第'+str(num)+'次',end='')
@@ -311,9 +242,7 @@ print("请稍等，正在加载资源......")
 
 if is_admin():
     while True:
-        print('1. 魂土')
-        print('2. 御灵')
-        print('0. 退出')
+        help()
         flag = input('请选择模式：')
         if flag == '1':
             watu()
@@ -322,10 +251,10 @@ if is_admin():
         elif flag == '0':
             exit(0)
         else:
-            print('对叭起，我不认识它QAQ')
+            print('对叭起, 我不认识它QAQ')
 else:
     if sys.version_info[0] == 3:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
     else:  # in python2.x
-        #ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
         pass

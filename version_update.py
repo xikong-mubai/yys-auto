a8b344c5460caaa78590c87bb135e5586a735f98
xikong-mubai <kirito_xibai@qq.com>
2023-10-17 17:10:46 +0800

    update function finish

--- version_update.py
+++ version_update.py
@@ -1,8 +1,17 @@
 import win32process,win32api,win32con
+from ctypes import windll
+import requests,zipfile
+
+def is_admin():
+    try:
+        return windll.shell32.IsUserAnAdmin()
+    except Exception as e:
+        print('\r\n',e)
+        return False
+
+print("admin: ",is_admin())
 
 for i in win32process.EnumProcesses():
-    if i != 32852 and i != 22940:
-        continue
     try:
         process  = win32api.OpenProcess(
                         win32con.PROCESS_ALL_ACCESS,
@@ -12,7 +21,18 @@ for i in win32process.EnumProcesses():
                     )
         for j in win32process.EnumProcessModules(process):
             tmp_name = win32process.GetModuleFileNameEx(process,j)
-            print(i,j,tmp_name)
+            if 'yys.exe' in tmp_name:
+                print(i,j,tmp_name)
+                win32process.TerminateProcess(process,0)
     except Exception as e:
         #print(i,e)
         pass
+
+url = 'https://code.xibai.xyz/yys-auto.zip'
+r = requests.get(url,verify=False)
+yys_zip = open('./yys-auto.zip','wb')
+yys_zip.write(r.content)
+yys_zip.close()
+
+yys_zip_fd = zipfile.ZipFile('./yys-auto.zip')
+yys_zip_fd.extractall()
